3d0a075979fb5d20e44c57f10684ada5665b5f45
Madhav Lalit <lalitmadhav129@gmail.com>
2025-06-30 11:27:26 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-db', function() {
+    try {
+        DB::connection()->getPdo();
+        return "DB connected successfully!";
+    } catch (\Exception $e) {
+        return "DB connection failed: " . $e->getMessage();
+    }
+});
